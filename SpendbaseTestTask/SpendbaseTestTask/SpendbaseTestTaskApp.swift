fa51e4b5c71fede247d50897ddb5e6e579ee0bc9
OlehOliinykov <oleh38568@gmail.com>
2024-05-09 20:26:01 +0300

    initial commit

---
+++ SpendbaseTestTask/SpendbaseTestTask/SpendbaseTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendbaseTestTaskApp.swift
+//  SpendbaseTestTask
+//
+//  Created by Oleh Oliinykov on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpendbaseTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
